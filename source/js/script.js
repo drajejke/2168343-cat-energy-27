3f73b3ec4f8d0e15507a6e081fe6b71acca9557d
Denis Bardyshevskiy <denisport91@gmail.com>
2023-01-28 10:53:37 +0300

    удаляет папку билд и добавляет правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
 const navMain = document.querySelector('.main-navigation');
 const navToggle = document.querySelector('.main-navigation__toggle');
 
+navMain.classList.remove('main-navigation--nojs');
+
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-navigation--closed')) {
     navMain.classList.remove('main-navigation--closed');
